a36b3e1e34ffe0abfbfedc5fc51abd31f2bd73bf
tasosxak <tasosxak@hotmail.com>
2018-02-18 16:14:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup (
        packages=find_packages(),
 
        # Declare your packages' dependencies here, for eg:
-       install_requires=['foo>=3'],
+       install_requires=[''],
 
        # Fill in these to make your Egg ready for upload to
        # PyPI
@@ -16,7 +16,7 @@ setup (
        #summary = 'Just another Python package for the cheese shop',
        url='',
        license='',
-       long_description='Long description of the package',
+       long_description='',
 
        # could also include long_description, download_url, classifiers, etc.
 

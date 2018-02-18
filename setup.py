1c5aec298701037de72bc5cc7865fcdc2702399c
tasosxak <tasosxak@hotmail.com>
2018-02-18 16:15:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,10 @@ setup (
        name='Doer',
        version='0.1',
        packages=find_packages(),
-
-       # Declare your packages' dependencies here, for eg:
        install_requires=[''],
-
-       # Fill in these to make your Egg ready for upload to
-       # PyPI
        author='tasosxak',
        author_email='tasosxakx@gmail.com',
-
-       #summary = 'Just another Python package for the cheese shop',
        url='',
        license='',
        long_description='',
-
-       # could also include long_description, download_url, classifiers, etc.
-
-
        )
